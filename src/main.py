ff193887cf9eb35a5d6b89661821e0bc05005c0e
Quoc21 <np.anhquoc21@gmail.com>
2025-03-25 20:41:01 +0700

    delete post url

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from src.data_processing.data_preprocessing import preprocess_data
 if __name__ == '__main__':
     # region preprocess data
 
-    raw_data_path = CONFIG['raw_data_path']
-    preprocess_data(raw_data_path)
+    # raw_data_path = CONFIG['raw_data_path']
+    # preprocess_data(raw_data_path)
 
     # endregion
 
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     # Nhớ xoá thông tin khi push code
     # username = ''
     # password = ''
-    # post_url = 'https://www.facebook.com/share/p/1AbVq4VtFy/'
+    # post_url = ''
     # crawl_post_data(username, password, post_url)
     # endregion
 
